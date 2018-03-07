68d8eb0eb40bb2fa0686861a7bd1d858c26481ee
Larysa Koziarivska <ryssia@ukr.net>
2018-03-07 15:57:05 +0200

    Child Theme added

--- testdomain/htdocs/wp-content/themes/twentyseventeen/footer.php
+++ testdomain/htdocs/wp-content/themes/twentyseventeen/footer.php
@@ -37,51 +37,11 @@
 
 				get_template_part( 'template-parts/footer/site', 'info' );
 				?>
-				<div class="footer-contacts">
-					<div class="footer-contacts__wrap"><span>Call Us: <a href="tel:+38-098-765-4321"">0987654321</a></span><a class="btn" href="tel:+38-098-765-4321"">Call Us</a></div>
-					<div  class="footer-contacts__wrap"><span>Email: <a href="mailto:testdomain@mail.to">testdomain@mail.to</a></span><a class="btn" href="mailto:testdomain@mail.to">Email Us</a></div>
-					<button id="myBtn">Contact Us</button>
-				</div>
 			</div><!-- .wrap -->
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
 </div><!-- #page -->
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
-
-</div>
 <?php wp_footer(); ?>
-<script>
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
 </body>
 </html>
